917bff1e69ddba765899a5e4c154c41399df07cf
Hugo Renzzo Olcese <renzzo98@yahoo.com>
2022-10-05 01:04:40 -0400

    ver1.0 for launch is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cron/cron.dart';
+import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+const ow2Logo = "/images/ow2_logo.png";
+const loadingGif = "/images/loading_gif.gif";
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Overwatch 2 Experience',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +28,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Overwatch 2'),
     );
   }
 }
@@ -48,68 +54,212 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static List<String> searchingMsg = [
+    "SEARCHING FOR GAME SERVER...",
+    "IN QUEUE: 9000 PLAYERS AHEAD OF YOU",
+    "IN QUEUE: 10000 PLAYERS AHEAD OF YOU",
+    "IN QUEUE: 20000 PLAYERS AHEAD OF YOU",
+    "IN QUEUE: 30000 PLAYERS AHEAD OF YOU",
+    "IN QUEUE: 40000 PLAYERS AHEAD OF YOU",
+  ];
 
-  void _incrementCounter() {
+  String activeMsg = searchingMsg.elementAt(0);
+
+  String btn1 = "MANAGE ACCOUNT";
+  String btn2 = "EXIT TO DESKTOP";
+  static String oldMsg = "THIS IS NOT MINING BITCOIN. DO NOT WORRY";
+  static String newMsg = "YOU CANNOT LEAVE AT THIS TIME";
+  String botMsg = oldMsg;
+
+  void _changeWaitingMsg() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      //searchingMsg++;
+      var rng = Random();
+      var newMsg = rng.nextInt(6);
+      activeMsg = searchingMsg.elementAt(newMsg);
+      botMsg = oldMsg;
+    });
+  }
+
+  void _warnUser() {
+    setState(() {
+      botMsg = newMsg;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var cron = new Cron();
+    cron.schedule(new Schedule.parse('*/1 * * * *'), () async {
+      print('every one minutes');
+      _changeWaitingMsg();
+    });
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Color.fromARGB(255, 238, 225, 205),
+              Color.fromARGB(255, 174, 167, 152)
+            ])),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Text(
+                        '2.1.0.1 - 105964',
+                        style: GoogleFonts.lato(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 45, 45, 45)),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child: Image.asset(
+                    ow2Logo,
+                    height: 300,
+                    width: 600,
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 50.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Image.asset(
+                            loadingGif,
+                            height: 25,
+                            width: 25,
+                          ),
+                        ),
+                        Text(
+                          '$activeMsg',
+                          style: GoogleFonts.overpass(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 45, 45, 45)),
+                        ),
+                      ],
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 18, horizontal: 32),
+                      ),
+                      onPressed: () => _warnUser(),
+                      child: const Text(
+                        "CANCEL",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      )),
+                ),
+                const Expanded(child: Center()),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(200, 10),
+                                  backgroundColor: const Color(0xffede1a8),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 18, horizontal: 32),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  "$btn1",
+                                  style: GoogleFonts.lato(
+                                    fontSize: 12.0,
+                                  ),
+                                )),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(200, 10),
+                                  backgroundColor: const Color(0xffede1a8),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 18, horizontal: 32),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  "$btn2",
+                                  style: GoogleFonts.lato(
+                                    fontSize: 12.0,
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                    child: Text(
+                      '$botMsg',
+                      style: GoogleFonts.lato(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 45, 45, 45)),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 }
